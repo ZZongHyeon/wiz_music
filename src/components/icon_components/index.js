017a9d7d3f15faebc9c4b99be343b0153eb89cda
ZZongHyeon <112713623+ZZongHyeon@users.noreply.github.com>
2026-01-20 11:58:02 +0900

    공통 컴포넌트

---
+++ src/components/icon_components/index.js
@@ -0,0 +1,15 @@
+export { default as IconBase } from "./IconBase";
+
+export { default as ArrowIcon } from "./ArrowIcon";
+export { default as AwardIcon } from "./AwardIcon";
+export { default as BookIcon } from "./BookIcon";
+export { default as CalendarIcon } from "./CalendarIcon";
+export { default as InstagramIcon } from "./InstagramIcon";
+export { default as KakaoIcon } from "./KakaoIcon";
+export { default as MapPinIcon } from "./MapPinIcon";
+export { default as MessageIcon } from "./MessageIcon";
+export { default as MusicNoteIcon } from "./MusicNoteIcon";
+export { default as PhoneIcon } from "./PhoneIcon";
+export { default as PlayIcon } from "./PlayIcon";
+export { default as UserIcon } from "./UserIcon";
+export { default as VideoIcon } from "./VideoIcon";
